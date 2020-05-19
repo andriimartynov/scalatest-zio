757448893537c7dbbd5a523c315d6667c4146d7d
Andrii Martynov <andrii.martynov@gmail.com>
2020-05-19 17:30:47 +0300

    updated to 3.3.0-SNAP2

--- project/ScalaTest.scala
+++ project/ScalaTest.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object ScalaTest {
-  lazy val SCALATEST_VERSION = "3.1.2"
+  lazy val SCALATEST_VERSION = "3.3.0-SNAP2"
 
   def dependency: ModuleID =
     "org.scalatest" %% "scalatest" % SCALATEST_VERSION
