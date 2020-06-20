a555fd9eccc994b2ce0101025db97bf45cd092bb
Andrii Martynov <andrii.martynov@gmail.com>
2020-06-20 18:38:13 +0300

    scalatest updated to 3.2.0

--- project/ScalaTest.scala
+++ project/ScalaTest.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object ScalaTest {
-  lazy val SCALATEST_VERSION = "3.2.0-M4"
+  lazy val SCALATEST_VERSION = "3.2.0"
 
   def dependency: ModuleID =
     "org.scalatest" %% "scalatest" % SCALATEST_VERSION
